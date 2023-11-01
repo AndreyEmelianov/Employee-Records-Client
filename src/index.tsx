5e75fe1d366a4c8193581275128a7b8caa44ba88
Andrey Emelianov <621077@mail.ru>
2023-11-01 19:28:52 +0300

    [feat(pages)]: добавил пустые компоненты логина и регистрации и добавил в index.tsx browserRouter с этими компонентами

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { Paths } from './paths/paths';
 import { store } from './app/store';
+import Login from './pages/login';
+import Register from './pages/register';
+
 import reportWebVitals from './reportWebVitals';
+
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>home</h1>,
+  },
+  {
+    path: Paths.login,
+    element: <Login />,
+  },
+  {
+    path: Paths.register,
+    element: <Register />,
+  },
+]);
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -11,7 +33,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <h1>Hello</h1>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 );

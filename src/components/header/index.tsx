d9b1b8bbe30aa96f2d49a3ae57f81a960f4a4317
Andrey Emelianov <621077@mail.ru>
2023-11-02 21:20:21 +0300

    [feat(header)]: добавил отображение кнопок в хедере по условию, залогинен пользователь или нет

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,25 @@
 import { Layout, Space, Typography } from 'antd';
-import { LoginOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { LoginOutlined, LogoutOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
+import { Link, useNavigate } from 'react-router-dom';
 
 import Button from '../ui/button';
 import { Paths } from '../../paths/paths';
 
 import styles from './index.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from '../../features/auth/authSlice';
 
 const Header: React.FC = () => {
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onLogout = () => {
+    dispatch(logout());
+    localStorage.removeItem('employeeToken');
+    navigate('/login');
+  };
+
   return (
     <Layout.Header className={styles.header}>
       <Space>
@@ -18,19 +30,24 @@ const Header: React.FC = () => {
           </Button>
         </Link>
       </Space>
-
-      <Space>
-        <Link to={Paths.register}>
-          <Button type="ghost" icon={<UserOutlined />}>
-            Зарегистрироваться
-          </Button>
-        </Link>
-        <Link to={Paths.login}>
-          <Button type="default" icon={<LoginOutlined />}>
-            Войти
-          </Button>
-        </Link>
-      </Space>
+      {user ? (
+        <Button type="ghost" icon={<LogoutOutlined />} onClick={onLogout}>
+          Выйти
+        </Button>
+      ) : (
+        <Space>
+          <Link to={Paths.register}>
+            <Button type="ghost" icon={<UserOutlined />}>
+              Зарегистрироваться
+            </Button>
+          </Link>
+          <Link to={Paths.login}>
+            <Button type="default" icon={<LoginOutlined />}>
+              Войти
+            </Button>
+          </Link>
+        </Space>
+      )}
     </Layout.Header>
   );
 };

cc3c35a3fb13c917a9b1976b13d8dfd378e9338e
Andrey Emelianov <621077@mail.ru>
2023-11-02 20:55:04 +0300

    [feat(features)]: реализовал слайс сотрудников

---
+++ src/features/employees/employeesSlice.ts
@@ -0,0 +1,30 @@
+import { Employee } from '@prisma/client';
+import { createSlice } from '@reduxjs/toolkit';
+import { employeesApi } from '../../app/services/employees';
+import { RootState } from '../../app/store';
+
+interface IInitialState {
+  employees: Employee[] | null;
+}
+
+const initialState: IInitialState = {
+  employees: null,
+};
+
+const employeesSlice = createSlice({
+  name: '@@employees',
+  initialState,
+  reducers: {
+    logout: () => initialState,
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(employeesApi.endpoints.getAllEmployees.matchFulfilled, (state, action) => {
+      state.employees = action.payload;
+    });
+  },
+});
+
+export const { logout } = employeesSlice.actions;
+export default employeesSlice.reducer;
+
+export const selectEmployees = (state: RootState) => state.employees;

d09e7f6ed7cdadc06378cf920bc8b62bdcbb10c4
Andrey Emelianov <621077@mail.ru>
2023-11-07 16:35:29 +0300

    [feat(pages)]: реализовал страницу регистрации в приложении, проверил регистрация работает корректно

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -3,15 +3,45 @@ import Layout from '../../components/layout';
 import Input from '../../components/ui/input';
 import PasswordInput from '../../components/ui/password-input';
 import Button from '../../components/ui/button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Paths } from '../../paths/paths';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/auth/authSlice';
+import { useState } from 'react';
+import { useRegisterMutation } from '../../app/services/auth';
+import { User } from '@prisma/client';
+import { isErrorWithMessage } from '../../utils/is-error-with-message';
+import ErrorMessage from '../../components/error-message';
 
-const Register = () => {
+type RegisterData = Omit<User, 'id'> & { confirmPassword: string };
+
+const Register: React.FC = () => {
+  const [error, setError] = useState<string>('');
+  const navigate = useNavigate();
+
+  const user = useSelector(selectUser);
+  const [registerUser] = useRegisterMutation();
+
+  const register = async (data: RegisterData) => {
+    try {
+      await registerUser(data).unwrap();
+      navigate('/');
+    } catch (err) {
+      const maybeError = isErrorWithMessage(err);
+
+      if (maybeError) {
+        setError(err.data.message);
+      } else {
+        setError('Неизвестная ошибка');
+      }
+    }
+  };
+
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Зарегистрируйтесь" style={{ width: '30rem' }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={register}>
             <Input name="name" placeholder="Имя" />
             <Input type="email" name="email" placeholder="E-mail" />
             <PasswordInput name="password" placeholder="Пароль" />
@@ -24,6 +54,7 @@ const Register = () => {
             <Typography.Text>
               Уже зарегистрированны? <Link to={Paths.login}>Войти</Link>
             </Typography.Text>
+            <ErrorMessage message={error} />
           </Space>
         </Card>
       </Row>
